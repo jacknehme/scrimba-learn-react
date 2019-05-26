6224e0c2bc25cc0b392ed9ffe2dcf8a5767aadb0
jacknehme <jack.nehme@gmail.com>
2019-05-26 14:58:49 -0700

    Stopped at 19 props tutorial

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import TodoItem from './components/TodoItem';
+// import Header from './components/Header';
+// import MainContent from './components/MainContent';
+// import Footer from './components/Footer';
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <MainContent />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+function App(){
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
-  );
+  )
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,32 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// import App from './App';
-function App() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
-    const styles = {
-        fontSize: 30
-    };
-
-    if (hours < 12) {
-        timeOfDay = 'morning';
-        styles.color = "#04756F";
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = 'afternoon';
-        styles.color = "#2E0927";
-    } else {
-        timeOfDay = 'night';
-        styles.color = "#D90000";
-    }
-
-
-
-    return (
-        <h1 style={styles}>Good {timeOfDay}!</h1>
-    );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
