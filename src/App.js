2408da363c60ff8f9e64d9934c94a185c811fd64
jacknehme <jack.nehme@gmail.com>
2019-05-29 21:33:37 -0700

    Stopped at 30 Handling Events in React

--- src/App.js
+++ src/App.js
@@ -2,63 +2,129 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-/*
 
-Challenge:
-1. Convert all 3 components to be class-based
-2. Fix the small bug
 
-*/
 
-// import React from "react"
-// import ReactDOM from "react-dom"
+import TodoItem from './components/TodoItem';
+import todosData from './todosData';
 
-// #1
 class App extends React.Component {
-  render() {
+  constructor (){
+    super();
+    this.state= {
+      todos: todosData
+    };
+  }
+
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
     return (
-      <div>
-        <Header username="Jack"/>
-        <Greeting />
+      <div className="todo-list">
+        {todoItems}
       </div>
     )
   }
+
 }
 
-// #2
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <p>Welcome, {this.props.username}!</p>
-      </header>
-    )
-  }
-}
 
-// #3
-class Greeting extends React.Component {
 
-  timeOfDay() {
-    const date = new Date();
-    const hours = date.getHours();
-    if (hours < 12) {
-      return "morning";
-    } else if (hours >= 12 && hours < 17) {
-      return "afternoon";
-    } else {
-      return "night";
-    }
-  }
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
 
-  render() {
-    return <h1>Good {this.timeOfDay()} to you, sir or madam!</h1>
-  }
-}
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isLoginIn: true
+//     };
+//   }
+//   render() {
+//     let logged = this.state.isLoginIn ? 'in' : 'out';
+//     return (
+//       <div>
+//         <h1>You are currently logged {logged}</h1>
+//       </div>
+//     )
+//   }
+
+// }
+
+// Challenge:
+// Given an incomplete class-based component without a constructor, 
+// add a constructor and initialize state to fix the broken component.
+
+// class App extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = { name: 'Jack', age: 99 };
+//   }
+//   render(){
+//     return (
+//         <div>
+//             <h1>{this.state.name}</h1>
+//             <h3>{this.state.age} years old</h3>
+//         </div>
+//     )    
+//   }
+// }
+
+
+/*
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
+*/
+// import React from "react"
+// import ReactDOM from "react-dom"
+// #1
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Header username="Jack"/>
+//         <Greeting />
+//       </div>
+//     )
+//   }
+// }
+
+// // #2
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <p>Welcome, {this.props.username}!</p>
+//       </header>
+//     )
+//   }
+// }
+
+// // #3
+// class Greeting extends React.Component {
+
+//   timeOfDay() {
+//     const date = new Date();
+//     const hours = date.getHours();
+//     if (hours < 12) {
+//       return "morning";
+//     } else if (hours >= 12 && hours < 17) {
+//       return "afternoon";
+//     } else {
+//       return "night";
+//     }
+//   }
+
+//   render() {
+//     return <h1>Good {this.timeOfDay()} to you, sir or madam!</h1>
+//   }
+// }
+
 // import TodoItem from './components/TodoItem';
 // import todosData from './todosData';
-
-
 // function App() {
 //   const todoItemComponents = todosData.map(item => <TodoItem key={item.id} todo={item} />);
 //   return (
@@ -70,7 +136,6 @@ class Greeting extends React.Component {
 
 // import Product from './components/Product';
 // import productsData from './vschoolProducts';
-
 // function App () {
 //   const productComponents = productsData.map( product => <Product key={product.id} product={product} />);
 //   return (
@@ -82,7 +147,6 @@ class Greeting extends React.Component {
 
 // import Joke from './components/Joke';
 // import jokesData from './jokesData';
-
 // function App () {
 //   const jokeComponents = jokesData.map( joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />);  
 
@@ -96,7 +160,6 @@ class Greeting extends React.Component {
 // import Header from './components/Header';
 // import MainContent from './components/MainContent';
 // import Footer from './components/Footer';
-
 // function App() {
 //   return (
 //     <div>
@@ -108,7 +171,6 @@ class Greeting extends React.Component {
 // }
 
 // import TodoItem from './components/TodoItem';
-
 // function App(){
 //   return (
 //     <div className="todo-list">
